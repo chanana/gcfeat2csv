d7000b213c547bed26dc2db25c454d3e9b7bd02c
Shaurya Chanana <schanana@wisc.edu>
2018-05-16 18:09:02 -0500

    Skips duplicate filenames
    Added a list that is checked to skip duplicate files based solely on their filenames.

--- gcfeat2csv.py
+++ gcfeat2csv.py
@@ -7,12 +7,18 @@ mypath = "./gcfeat/"
 result_file = "Features.csv"
 onlyfiles = [mypath+f for f in listdir(mypath) if f != ".DS_Store"]
 pattern = re.compile("\d+-\d+")
+filenames = []
 
 for f in onlyfiles:
     # Get sample name (XX-XX)
     filename = re.search(pattern=pattern, string=f)
-    print(filename.group(0))
-
+    if filename.group(0) in filenames:
+        print("Duplicate File! Skipping {}".format(filename.group(0)))
+        continue
+    else:
+        filenames.append(filename.group(0))
+        print(filename.group(0))
+    
     # open connection to database file; select columns
     conn = sqlite3.connect(f)
     c = conn.cursor()
@@ -26,12 +32,12 @@ for f in onlyfiles:
         
         # add sample name to a row
         for tuple_row in data:
-            # mz_rt = 'mz'_'apex_intensity'
-            # mz and rt rounded to 4 decimal places
-            mz_rt = str(round(tuple_row[0], 4)) + "_" + str(round(tuple_row[2], 4)) # make mz_rt pair
+            # mz_rt = 'mz'_'apex_rt'
+            # mz and apex_rt rounded to 4 decimal places
+            # mz_rt = str(round(tuple_row[0], 4)) + "_" + str(round(tuple_row[2], 4)) # make mz_rt pair
             
-            # ['xx-xx', mz_rt, apex_intensity]
-            row = [filename.group(0), mz_rt, tuple_row[1]]
+            # ['xx-xx', mz, rt, apex_intensity]
+            row = [filename.group(0), round(tuple_row[0],4), round(tuple_row[2],2), round(tuple_row[1],4)]
             
             # write this row to the csv file
             fwriter.writerow(row)
